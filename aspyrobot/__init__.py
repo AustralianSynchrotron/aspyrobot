7998b5fc8cb4e0a2d5975dc2b2806f5bf766c616
RobbieClarken <robbie.clarken@gmail.com>
2018-01-25 18:43:38 +1100

    Bump version to 0.17.0

--- aspyrobot/__init__.py
+++ aspyrobot/__init__.py
@@ -2,6 +2,6 @@ from .robot import Robot
 from .server import RobotServer
 from .client import RobotClient
 
-__version__ = '0.16.1'
+__version__ = '0.17.0'
 
 __all__ = [Robot, RobotServer, RobotClient]
